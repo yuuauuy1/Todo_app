77ff8966f6c1a32f9caa503a1aeb8bf88f463383
yuuauuy1 <shiroropi.0511@outlook.jp>
2023-03-23 17:23:39 +0900

    new

---
+++ app.py
@@ -0,0 +1,66 @@
+from datetime import datetime, date
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+#DBの詳細
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == 'GET':
+        posts = Post.query.order_by(Post.due).all()
+        return render_template('index.html', posts=posts, today=date.today()) 
+    else:
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
+
+        due = datetime.strptime(due, '%Y-%m-%d')
+        new_post = Post(title=title, detail=detail, due = due)
+
+        #dbへの保存
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/')# '/'はトップページに戻る
+
+@app.route('/create')
+def create():
+    return render_template("create.html")
+
+@app.route('/detail/<int:id>')
+def read(id):
+    post = Post.query.get(id) #該当する投稿内容のidを取得して、detail.htmlに渡す
+    return render_template('detail.html', post = post)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Post.query.get(id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post = post)
+    else:
+        post.title = request.form.get('title')
+        post.detail = request.form.get('detail')
+        post.due = datetime.strptime(request.form.get('due'), '%Y-%m-%d')
+
+        db.session.commit()
+        return redirect('/')
+    
+if __name__ == '__main__':
+    app.run(debug = True)
